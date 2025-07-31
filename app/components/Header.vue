e54c3aa2a3006cef92b60181dc6aea1822dace4c
Carbonnel <antoine.carbonnel@airweb.de>
2025-07-31 18:55:37 +0200

    Add Tailwind CSS, default layout, and header component
    Introduce Tailwind CSS configuration for styling and create a default layout with a reusable header component. Replace the default app structure with the new layout system and include a placeholder logo asset.

---
+++ app/components/Header.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+	<header class="p-4 border-b-2">
+		<NuxtLink to="/" class="inline-block">
+			<img
+					src="/assets/logo.png"
+					alt="Logo du site"
+					class="w-[146px]"
+			/>
+		</NuxtLink>
+	</header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Header from "~/components/Header.vue";
+
+</script>
+
+<template>
+	<Header/>
+	<main>
+		<slot/>
+	</main>
+</template>
